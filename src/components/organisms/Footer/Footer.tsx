9b68b9633845d5f9b5e7ef7200f4ca85434442ad
k_ozawa <kouta.ozawa@nadia.bz>
2025-07-04 16:18:43 +0900

    コンポネか

---
+++ src/components/organisms/Footer/Footer.tsx
@@ -0,0 +1,216 @@
+import React from 'react'
+import clsx from 'clsx'
+
+export interface Props {
+  /** クラス名 */
+  className?: string
+}
+
+export const Footer: React.FC<Props> = ({ className }) => {
+  return (
+    <footer className="l-footer">
+      <div className="c-to-top js-to-top">
+        <a className="c-to-top__button">
+          <img
+            src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+            width="16"
+            alt="PAGE TOP"
+            data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/arrow_top.svg"
+            decoding="async"
+            className="lazyload"
+          /><noscript
+          ><img
+              src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/arrow_top.svg"
+              width="16"
+              alt="PAGE TOP"
+              data-eio="l"
+            /></noscript
+          >
+        </a>
+      </div>
+
+      <nav className="l-footer__nav">
+        <ul className="l-footer__menu">
+          <li>
+            <a href="https://tat1234.xsrv.jp/faq/" target="_blank">
+              <img
+                className="icon lazyload"
+                src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+                width="16"
+                alt=""
+                data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_faq.svg"
+                decoding="async"
+              /><noscript
+              ><img
+                  className="icon"
+                  src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_faq.svg"
+                  width="16"
+                  alt=""
+                  data-eio="l"
+                /></noscript
+              >
+              よくある質問
+            </a>
+          </li>
+          <li>
+            <a href="https://tat1234.xsrv.jp/price/" target="_blank">
+              <img
+                className="icon lazyload"
+                src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+                width="16"
+                alt=""
+                data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_price.svg"
+                decoding="async"
+              /><noscript
+              ><img
+                  className="icon"
+                  src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_price.svg"
+                  width="16"
+                  alt=""
+                  data-eio="l"
+                /></noscript
+              >
+              料金表
+            </a>
+          </li>
+          <li>
+            <a href="https://tat1234.xsrv.jp/artist-list/" target="_blank">
+              <img
+                className="icon lazyload"
+                src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+                width="16"
+                alt=""
+                data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_artist.svg"
+                decoding="async"
+              /><noscript
+              ><img
+                  className="icon"
+                  src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_artist.svg"
+                  width="16"
+                  alt=""
+                  data-eio="l"
+                /></noscript
+              >
+              アーティスト一覧
+            </a>
+          </li>
+          <li>
+            <a href="https://tat1234.xsrv.jp/recruit/" target="_blank">
+              <img
+                className="icon lazyload"
+                src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+                width="16"
+                alt=""
+                data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_recruit.svg"
+                decoding="async"
+              /><noscript
+              ><img
+                  className="icon"
+                  src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_recruit.svg"
+                  width="16"
+                  alt=""
+                  data-eio="l"
+                /></noscript
+              >
+              採用情報
+            </a>
+          </li>
+          <li>
+            <a href="https://tat1234.xsrv.jp/removal/" target="_blank">
+              <img
+                className="icon lazyload"
+                src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+                width="16"
+                alt=""
+                data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_removal.svg"
+                decoding="async"
+              /><noscript
+              ><img
+                  className="icon"
+                  src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_removal.svg"
+                  width="16"
+                  alt=""
+                  data-eio="l"
+                /></noscript
+              >
+              レーザー除去
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSePthoLh8ghv633J_znYgPGXDfQ7YyYu6E7gPrtWbg-WJYUrg/viewform"
+              target="_blank"
+            >
+              <img
+                className="icon lazyload"
+                src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+                width="16"
+                alt=""
+                data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_letter.svg"
+                decoding="async"
+              /><noscript
+              ><img
+                  className="icon"
+                  src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_letter.svg"
+                  width="16"
+                  alt=""
+                  data-eio="l"
+                /></noscript
+              >
+              メディア関係の方へ
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="l-footer__logo">
+        <img
+          className="c-image--responsive lazyload"
+          src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+          width="145"
+          alt="THE ARTMAKE TOKYO（ジ・アートメイク東京）"
+          data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/logo_tat_white.svg"
+          decoding="async"
+        /><noscript
+        ><img
+            className="c-image--responsive"
+            src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/logo_tat_white.svg"
+            width="145"
+            alt="THE ARTMAKE TOKYO（ジ・アートメイク東京）"
+            data-eio="l"
+          /></noscript
+        >
+      </div>
+
+      <ul className="l-footer__clinic-list">
+        <li>
+          <a href="https://tat1234.xsrv.jp/brow/ginza">銀座院</a>
+        </li>
+        <li>
+          <a href="https://tat1234.xsrv.jp/brow/osaka">大阪院</a>
+        </li>
+        <li>
+          <a href="https://tat1234.xsrv.jp/brow/nagoya">名古屋院</a>
+        </li>
+        <li>
+          <a href="https://tat1234.xsrv.jp/brow/yokohama">横浜院</a>
+        </li>
+        <li>
+          <a href="https://tat1234.xsrv.jp/brow/fukuoka">福岡院</a>
+        </li>
+      </ul>
+
+      <p className="l-footer__copyright">
+        &copy; 2025 THE ARTMAKE TOKYO. All Rights Reserved.
+      </p>
+
+      <ul className="l-footer__submenu">
+        <li className="l-footer__submenu-item">
+          <a href="https://tat1234.xsrv.jp/p_policy/" target="_blank"
+          >プライバシーポリシー</a>
+        </li>
+      </ul>
+    </footer>
+
+  )
+}
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -9,10 +9,138 @@ export interface Props {
 
 export const Header: React.FC<Props> = ({ className }) => {
   return (
-    <header className={clsx(styles['wrapper'], className)}>
-      <h1 className={clsx(styles.h1, 'is-pc font-24-28')}>
-        <span>ヘッダー</span>
-      </h1>
+    <header className="l-header js-header">
+      <p className="l-header__logo">
+        <a href="https://tat1234.xsrv.jp/">
+          <img
+            src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+            width="184"
+            alt="THE ARTMAKE TOKYO（ジ・アートメイク東京）"
+            data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/logo_tat.svg"
+            decoding="async"
+            className="lazyload"
+          />
+          <noscript>
+            <img
+              src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/logo_tat.svg"
+              width="184"
+              alt="THE ARTMAKE TOKYO（ジ・アートメイク東京）"
+              data-eio="l"
+            />
+          </noscript>
+        </a>
+      </p>
+
+      <div className="l-header__open js-nav-open"><span></span><span></span></div>
+
+      <nav className="l-header__nav js-nav">
+        <div className="l-header__logo u-sp">
+          <img
+            src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+            width="145"
+            alt="THE ARTMAKE TOKYO（ジ・アートメイク東京）"
+            data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/logo_tat.svg"
+            decoding="async"
+            className="lazyload"
+          />
+          <noscript>
+            <img
+              src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/logo_tat.svg"
+              width="145"
+              alt="THE ARTMAKE TOKYO（ジ・アートメイク東京）"
+              data-eio="l"
+            />
+          </noscript>
+        </div>
+        <div className="l-header__close js-nav-close">
+          <span></span><span></span>
+        </div>
+
+        <div className="l-header__menu-heading u-pc"><span>MENU</span></div>
+        <ul className="l-header__list">
+          <li><a href="#about">THE ARTMAKE TOKYOの人気スタイル</a></li>
+
+          <li><a href="#choice">クリニック選び</a></li>
+          <li><a href="#case">眉アートメイク症例</a></li>
+
+          <li><a href="#commitment">THE ARTMAKE TOKYOのこだわり</a></li>
+          <li><a href="#artist">アーティスト一覧</a></li>
+
+
+          <li><a href="#doctor">院長紹介</a></li>
+
+          <li><a href="#feature">THE ARTMAKE TOKYOの特徴</a></li>
+          <li><a href="#price">料金表</a></li>
+          <li><a href="#reason">選ばれる理由</a></li>
+          <li><a href="#steps">施術当日の流れ</a></li>
+
+          <li><a href="#clinic">クリニック情報</a></li>
+          <li><a href="#access">アクセス</a></li>
+
+          <li><a href="#qa">Q&A</a></li>
+        </ul>
+
+        <ul className="l-header__submenu">
+          <li>
+            <a href="https://tat1234.xsrv.jp/p_policy/" target="_blank"
+            >プライバシーポリシー
+            </a>
+          </li>
+          <li className="js-to-top"><a href="#">TOPへ戻る</a></li>
+        </ul>
+
+        <ul className="l-header__sns">
+          <li className="insta">
+            <a
+              href="https://www.instagram.com/the_artmake_tokyo/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img
+                src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+                width="20"
+                alt="Instagram"
+                data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_sns_insta.svg"
+                decoding="async"
+                className="lazyload"
+              /><noscript
+              ><img
+                  src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_sns_insta.svg"
+                  width="20"
+                  alt="Instagram"
+                  data-eio="l"
+                /></noscript
+              >
+            </a>
+          </li>
+          <li className="tiktok">
+            <a
+              href="https://www.tiktok.com/@theartmaketokyo"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img
+                src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+                width="17"
+                alt="tiktok"
+                data-src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_sns_tiktok.svg"
+                decoding="async"
+                className="lazyload"
+              />
+              <noscript>
+                <img
+                  src="https://tat1234.xsrv.jp/wp/wp-content/themes/the-artmake-tokyo/assets/images/common/icon_sns_tiktok.svg"
+                  width="17"
+                  alt="tiktok"
+                  data-eio="l"
+                />
+              </noscript>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
+
+
   )
 }
